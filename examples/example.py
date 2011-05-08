e67660039c35134790cb9709cd1c09b11e65d409
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2011-05-08 03:35:37 +0200

    Update example.py to use pygi

--- examples/example.py
+++ examples/example.py
@@ -5,15 +5,22 @@ example.py
 Created in 2010 by Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
 This work is placed in the public domain.
 """
-import gtk
-import keybinder
 
-def callback(user_data):
-	print "Handling", user_data
-	gtk.main_quit()
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Keybinder', '3.0')
+
+from gi.repository import Gtk
+from gi.repository import Keybinder
+
+def callback(keystr, user_data):
+	print "Handling", keystr, user_data
+	print "Event time:", Keybinder.get_current_event_time()
+	Gtk.main_quit()
 
 if __name__ == '__main__':
 	keystr = "<Ctrl>A"
-	keybinder.bind(keystr, callback, "Keystring %s (user data)" % keystr)
+	Keybinder.init()
+	Keybinder.bind(keystr, callback, "Keystring %s (user data)" % keystr)
 	print "Press", keystr, "to handle keybinding and quit"
-	gtk.main()
+	Gtk.main()
